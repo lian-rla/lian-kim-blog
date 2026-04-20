91dca2bffc785271ece2df0306f425cdcbfbfdb6
lian-rla <minjae260314@gmail.com>
2026-04-21 00:48:20 +0900

    2026/4/20 수정

--- src/components/information/index.js
+++ src/components/information/index.js
@@ -8,36 +8,14 @@ function Information({ author }) {
 
   return (
     <div className="information">
-      <div className="information-meta">
-        {bio.email && (
-          <div className="information-meta-item">
-            <span className="meta-icon">✉</span>
-            <span>{bio.email}</span>
-          </div>
-        )}
-        {bio.residence && (
-          <div className="information-meta-item">
-            <span className="meta-icon">📍</span>
-            <span>{bio.residence}</span>
-          </div>
-        )}
-        {bio.bachelorDegree && (
-          <div className="information-meta-item">
-            <span className="meta-icon">🎓</span>
-            <span>{bio.bachelorDegree}</span>
-          </div>
-        )}
+      <div className="information-image">
+        <Image style={{ width: 180, height: 180, borderRadius: '50%' }} src={bio.thumbnail} alt="profile" />
       </div>
-      <div className="information-profile">
-        <div className="information-image">
-          <Image style={{ width: 180, height: 180, borderRadius: '50%' }} src={bio.thumbnail} alt="profile" />
-        </div>
-        <div className="information-text">
-          <div className="information-name">{name}</div>
-          <div className="information-hanja">
-            <span>珉 옥돌 <b>민</b></span>
-            <span>材 재목 <b>재</b></span>
-          </div>
+      <div className="information-text">
+        <div className="information-name">{name}</div>
+        <div className="information-hanja">
+          <span>珉 <b>민</b>옥돌 <b>민</b></span>
+          <span>材 <b>재</b>재목 <b>재</b></span>
         </div>
       </div>
     </div>
